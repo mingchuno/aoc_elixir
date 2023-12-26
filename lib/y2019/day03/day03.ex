1ee41c3be1a28b855ce4885dab5b3a40e769d9d0
mingchuno <mingchuno@gmail.com>
2023-12-27 01:52:11 +0800

    day 3 part 2

--- lib/y2019/day03/day03.ex
+++ lib/y2019/day03/day03.ex
@@ -9,6 +9,44 @@ defmodule Y2019.Day03.Day03 do
     |> Enum.min()
   end
 
+  def part_2(input_file) do
+    AocFile.read_input_file(input_file)
+    |> Enum.map(&parse_line/1)
+    |> Enum.map(fn path -> walk_line_2(path, {0, 0}, 0, %{}) end)
+    |> Enum.reduce(fn m1, m2 -> Map.intersect(m1, m2, &reduce_intersection/3) end)
+    |> Map.values()
+    |> Enum.filter(&(&1 != 0))
+    |> Enum.min()
+  end
+
+  defp reduce_intersection(_k, v1, v2), do: v1 + v2
+
+  defp walk_line_2([], _coord, _current_step, map), do: map
+
+  defp walk_line_2(path, {x, y}, current_step, map) do
+    [{direction, steps} | rest] = path
+
+    {x_end, y_end} =
+      case direction do
+        :up -> {x, y - steps}
+        :down -> {x, y + steps}
+        :left -> {x - steps, y}
+        :right -> {x + steps, y}
+      end
+
+    new_map =
+      for(i <- x..x_end, j <- y..y_end, do: {i, j})
+      |> Enum.with_index(current_step)
+      |> Enum.into(%{})
+
+    walk_line_2(
+      rest,
+      {x_end, y_end},
+      current_step + steps,
+      Map.merge(map, new_map, fn _k, v1, v2 -> min(v1, v2) end)
+    )
+  end
+
   defp walk_line(steps) do
     {_, set} = Enum.reduce(steps, {{0, 0}, MapSet.new()}, &walk_step/2)
     set
--- test/y2019/day03/day03_test.exs
+++ test/y2019/day03/day03_test.exs
@@ -8,4 +8,12 @@ defmodule Y2019.Day03.Day02Test do
   test "part 1 real" do
     assert Y2019.Day03.Day03.part_1("./inputs/2019/day3-real.txt") == 731
   end
+
+  test "part 2 example" do
+    assert Y2019.Day03.Day03.part_2("./inputs/2019/day3-example.txt") == 30
+  end
+
+  test "part 2 real" do
+    assert Y2019.Day03.Day03.part_2("./inputs/2019/day3-real.txt") == 5672
+  end
 end
