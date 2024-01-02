de905449f1637be481c1c1138dda1f3dd83a095a
mingchuno <mingchuno@gmail.com>
2024-01-02 23:07:29 +0800

    day 9 part 2

--- lib/y2019/day09/day09.ex
+++ lib/y2019/day09/day09.ex
@@ -3,11 +3,10 @@ alias Y2019.Computer.Computer
 
 defmodule Y2019.Day09.Day09 do
   def part_1(input_file) do
-    program = read_input_file(input_file) |> Computer.parse()
-
-    hd(Computer.compute(program, [1]) |> IO.inspect())
+    read_input_file(input_file) |> Computer.parse() |> Computer.compute([1]) |> hd
   end
 
   def part_2(input_file) do
+    read_input_file(input_file) |> Computer.parse() |> Computer.compute([2]) |> hd
   end
 end
--- test/y2019/day09/day09_test.exs
+++ test/y2019/day09/day09_test.exs
@@ -7,7 +7,7 @@ defmodule Y2019.Day09.Day09Test do
     assert Day09.part_1("./inputs/2019/day9-real.txt") == 3_429_606_717
   end
 
-  # test "part 2" do
-  #   assert Day09.part_2("./inputs/2019/day9-real.txt") == 12_077_198
-  # end
+  test "part 2" do
+    assert Day09.part_2("./inputs/2019/day9-real.txt") == 33_679
+  end
 end
